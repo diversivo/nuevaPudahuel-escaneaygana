c4f27b8cc9399cc48a2026d029579910b292b560
Daniel Quinteros <dquinteros@diversivo.cl>
2020-11-22 23:23:37 -0300

    working animation

--- src/pages/ruleta.js
+++ src/pages/ruleta.js
@@ -4,251 +4,157 @@ import MomentumSlider from 'momentum-slider';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import testImage from '../assets/images/ruleta_test.png';
 
 const Streaming = ({ location }) => {
 
   // Variables to use later
   // var root = document.documentElement;
   const container = useRef(null);
-  const pagination = useRef(null);
-  // const [currentIndex, setCurrentIndex] = useState(29);
-  const initialIndex = 6;
-  const maxRange = 3;
-  const range = [0, 11];
+  const [imgSlider, setImgSlider] = useState(null);
+  const initialIndex = 5;
+  const minMovement = 30;
+  const winner = 3;
+  const loop = 2;
+  const range = [0, 9];
 
-  Number.prototype.between = function (a, b) {
-    var min = Math.min(a, b),
-      max = Math.max(a, b);
-    return this > min && this < max;
-  };
-  // var container = document.querySelector('.container');
-  // var button = document.querySelector('.button');
-  // var counter = document.querySelector('.counter');
-  // var running = false;
-  // var timer = null;
-  // var seconds = 0;
-  // var secondsInitial = 0;
-  // Initializing the slider
+  const titles = [
+    '40% de descuento en 1 cosas',
+    '40% de descuento en 2 cosas',
+    '40% de descuento en 3 cosas',
+    '40% de descuento en 4 cosas',
+    '40% de descuento en 5 cosas',
+    '40% de descuento en 6 cosas',
+    '40% de descuento en 7 cosas',
+    '40% de descuento en 8 cosas',
+    '40% de descuento en 9 cosas',
+    '40% de descuento en 10 cosas'
+  ];
 
-  let ms = null;
+  const legals = [
+    'Hasta agotar stock.',
+    'Hasta agotar stock.',
+    'Hasta agotar stock.',
+    'Hasta agotar stock.',
+    'Hasta agotar stock.',
+    'Hasta agotar stock.',
+    'Hasta agotar stock.',
+    'Hasta agotar stock.',
+    'Hasta agotar stock.',
+    'Hasta agotar stock.',
+  ];
+
+  const texts = titles.reduce((acum, current, index) => {
+    return [...acum, {
+      title: current,
+      legal: legals[index]
+    }]
+  }, [])
+
+  const images = [
+    testImage,
+    testImage,
+    testImage,
+    testImage,
+    testImage,
+    testImage,
+    testImage,
+    testImage,
+    testImage,
+    testImage,
+  ];
+
+
+
+
   useEffect(() => {
     const slidersContainer = container.current;
-    // Initializing the numbers slider
-    var msNumbers = new MomentumSlider({
-      el: slidersContainer,
-      cssClass: 'ms--numbers',
-      range,
-      loop: 2,
-      rangeContent: function (i) {
-        return '0' + i;
-      },
-      style: {
-        transform: [{ scale: [0.4, 1] }],
-        opacity: [0, 1]
-      },
-      interactive: false
-    });
 
     // Initializing the titles slider
-    var titles = [
-      'King of the Ring Fight',
-      'Sound of Streets',
-      'Urban Fashion',
-      'Windy Sunset',
-      'King of the Ring Fight',
-      'Sound of Streets',
-      'Urban Fashion',
-      'Windy Sunset',
-      'King of the Ring Fight',
-      'Sound of Streets',
-      'Urban Fashion',
-      'Windy Sunset',
-    ];
+
     var msTitles = new MomentumSlider({
       el: slidersContainer,
       cssClass: 'ms--titles',
       range,
       rangeContent: function (i) {
-        return '<h3>' + titles[i] + '</h3>';
+        return `<h3>${texts[i].title} </h3>
+        <span>${texts[i].legal}</span>`;
       },
-      vertical: true,
-      reverse: true,
+      loop,
+      // reverse: true,
       style: {
-        opacity: [0, 1]
+        opacity: [0.3, 1]
       },
+      vertical: false,
       interactive: false
     });
-
-    // Initializing the links slider
-    var msLinks = new MomentumSlider({
-      el: slidersContainer,
-      cssClass: 'ms--links',
-      range,
-      rangeContent: function () {
-        return '<a class="ms-slide__link">View Case</a>';
-      },
-      vertical: true,
-      interactive: false
-    });
-
-    // Get pagination items
-    var pagination = document.querySelector('.pagination');
-    var paginationItems = [].slice.call(pagination.children);
 
     // Initializing the images slider
-    var msImages = new MomentumSlider({
+    const msImages = new MomentumSlider({
       // Element to append the slider
       el: slidersContainer,
       // CSS class to reference the slider
       cssClass: 'ms--images',
       // Generate the 4 slides required
       currentIndex: initialIndex,
+      loop,
       range,
-      rangeContent: function () {
-        return '<div class="ms-slide__image-container"><div class="ms-slide__image"></div></div>';
+      rangeContent: function (index) {
+        console.log(images[index]);
+        return `<div class="ms-slide__image-container"><div class="ms-slide__image" style="background-image:url('${images[index]}')"></div></div>`;
       },
       // Syncronize the other sliders
-      sync: [msNumbers, msTitles, msLinks],
+      sync: [msTitles],
       // Styles to interpolate as we move the slider
       style: {
         '.ms-slide__image': {
-          transform: [{ scale: [1.5, 1] }],
+          transform: [{ scale: [1, 1.1] }],
           opacity: [0.1, 1]
         }
       },
       // Update pagination if slider change
       change: function (newIndex, oldIndex) {
-        if (typeof oldIndex !== 'undefined') {
-          paginationItems[oldIndex].classList.remove('pagination__item--active');
+        if (newIndex && oldIndex) {
+          const toLeft = goLeft(newIndex, oldIndex);
+          console.log('Left?');
+          console.log(toLeft)
+          animate(msImages, toLeft, winner, minMovement)
+          console.log(oldIndex);
+          console.log(newIndex);
+          console.log('==========');
         }
-        paginationItems[newIndex].classList.add('pagination__item--active');
       }
     });
 
-    // Select corresponding slider item when a pagination button is clicked
-    pagination.addEventListener('click', function (e) {
-      if (e.target.matches('.pagination__button')) {
-        var index = paginationItems.indexOf(e.target.parentNode);
-        msImages.select(index);
-      }
-    });
-
-    // while(ms.getCurrentIndex()<59){
-    //   ms.next();
-    //   console.log(ms.getCurrentIndex());
-    // }
-
-    // setInterval(()=>ms.next(),10);
-
-
-    // setTimeout(()=>{
-    //   ms.next();
-    //   console.log(ms.getCurrentIndex());
-    // },2000)
-
-
-    container.current.addEventListener('mouseup', () => {
-      setTimeout(() => {
-        setInterval(() => animationinterval(msImages), 50);
-      }, 50)
-    });
+    setImgSlider(msImages);
   }, []);
 
-  const animationinterval = (ms) => {
-    const index = ms.getCurrentIndex();
+  const goLeft = (newIndex, oldIndex) =>
+    newIndex > oldIndex ?
+      newIndex - oldIndex === 1 :
+      oldIndex - newIndex !== 1;
 
-    const ii = initialIndex;
-    if (index.between(ii, ii - maxRange)) {
-      ms.prev();
-    } else if (index.between(ii, ii + maxRange)) {
-      ms.next();
-    } else if (index != initialIndex) {
-      ms.disable();
+  let animation = null;
+
+  const animate = (slider, toLeft, stopConditon, minIterations) => {
+    if (!animation) {
+      let iterations = 0;
+      const interval = setInterval(() => {
+        toLeft ? slider.next() : slider.prev();
+        iterations += 1;
+        if (slider.getCurrentIndex() === stopConditon && iterations >= minIterations) {
+          clearInterval(interval);
+          slider.disable();
+        }
+      }, 100);
+      animation = interval
     }
   };
 
-  // useEffect(() => {
-  //   if(ms){
-  //     ms.select(2);
-
-  //   }
-  // }, [currentIndex]);
-
-  // // Simple toggle functionality
-  // button.addEventListener('click', function () {
-  //   if (running) {
-  //     stop();
-  //   } else {
-  //     start();
-  //   }
-  //   running = !running;
-  // });
-
-  // Start the countdown
-  // function start() {
-  //   // Disable the slider during countdown
-  //   ms.disable();
-  //   // Get current slide index, and set initial values
-  //   seconds = ms.getCurrentIndex() + 1;
-  //   counter.innerText = secondsInitial = seconds;
-  //   root.style.setProperty('--progress', 0);
-  //   // Add class to trigger CSS transitions for `running` state
-  //   container.classList.add('container--running');
-  //   // Set interval to update the component every second
-  //   timer = setInterval(function () {
-  //     // Update values
-  //     counter.innerText = --seconds;
-  //     root.style.setProperty('--progress', (secondsInitial - seconds) / secondsInitial * 100);
-  //     // Stop countdown if it's finished
-  //     if (!seconds) {
-  //       stop();
-  //       running = false;
-  //     }
-  //   }, 1000);
-  // }
-
-  // Stop the countdown
-  // function stop() {
-  //   // Enable slider
-  //   ms.enable();
-  //   // Clear interval
-  //   clearInterval(timer);
-  //   // Reset progress
-  //   root.style.setProperty('--progress', 100);
-  //   // Remove `running` state
-  //   container.classList.remove('container--running');
-  // }
-
   return (<Layout>
     <SEO title="Desliza y gana - Nueva Pudahuel" />
-    <div className="container">
-      <main ref={container} className="sliders-container">
-        <ul ref={pagination} className="pagination">
-          <li className="pagination__item"><a className="pagination__button"></a></li>
-          <li className="pagination__item"><a className="pagination__button"></a></li>
-          <li className="pagination__item"><a className="pagination__button"></a></li>
-          <li className="pagination__item"><a className="pagination__button"></a></li>
-          <li className="pagination__item"><a className="pagination__button"></a></li>
-          <li className="pagination__item"><a className="pagination__button"></a></li>
-          <li className="pagination__item"><a className="pagination__button"></a></li>
-          <li className="pagination__item"><a className="pagination__button"></a></li>
-          <li className="pagination__item"><a className="pagination__button"></a></li>
-          <li className="pagination__item"><a className="pagination__button"></a></li>
-          <li className="pagination__item"><a className="pagination__button"></a></li>
-          <li className="pagination__item"><a className="pagination__button"></a></li>
-        </ul>
-      </main>
-      <footer className="footer">
-        <nav className="footer__menu">
-          <ul className="footer__menu__list">
-            <li className="footer__menu__item"><a className="footer__menu__link">facebook</a></li>
-            <li className="footer__menu__item"><a className="footer__menu__link">dribbble</a></li>
-            <li className="footer__menu__item"><a className="footer__menu__link">instagram</a></li>
-          </ul>
-        </nav>
-      </footer>
-    </div>
-
+    <main ref={container} className="sliders-container" />
+    <button className="spin-btn" onClick={() => animate(imgSlider, true, winner, minMovement)}>GIRAR RULETA</button>
   </Layout>);
 }
 
